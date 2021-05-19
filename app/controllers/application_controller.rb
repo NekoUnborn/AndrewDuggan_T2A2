2d9ea6746a6b49be218fbf8e9ba9abe0d3b47f90
neko <neko@MyTUFLaptop.localdomain>
2021-05-19 10:46:56 +1000

    Added Pundit Policies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+  include Pundit
+rescue_from Pundit:NotAuthorisedError, with: :forbidden
 private
+  def forbidden
+    flash[:alert] = 'you are not authorised to perform that action'
+    redirect_to request.referrer || root_path
+  end
 
 end
